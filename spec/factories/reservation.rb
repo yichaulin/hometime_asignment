f3524d4396e330f0a2e804387b2a25a6e2e9b98d
Max <max.lin@screeningeagle.com>
2021-11-17 15:52:20 +0800

    Add update behavior tests

---
+++ spec/factories/reservation.rb
@@ -0,0 +1,22 @@
+FactoryBot.define do
+  factory :reservation do
+    client { "Airbnb" }
+    code { "YYY12345678" }
+    start_date { "2021-04-14" }
+    end_date { "2021-04-18" }
+    nights { 4 }
+    guests { 4 }
+    adults { 2 }
+    children { 2 }
+    infants { 0 }
+    status { "accepted" }
+    guest_first_name { "Wayne" }
+    guest_last_name { "Woodbridge" }
+    guest_phone_numbers { "639123456789" }
+    guest_email { "wayne_woodbridge@bnb.com" }
+    currency { "AUD" }
+    payout_amount { 4200.00 }
+    security_amount { 500 }
+    total_amount { 4700.00 }
+  end
+end
--- spec/services/reservation_service_spec.rb
+++ spec/services/reservation_service_spec.rb
@@ -4,7 +4,7 @@ require 'shared_payload'
 RSpec.describe ReservationService, type: :model do
   include_context "shared payload"
 
-  context 'Success cases:' do
+  context 'Create Reservation:' do
     it 'should verify and create reservation with payload#1' do
       params = JSON.parse(payload1, symbolize_names: true)
       rsv = ReservationService.new(params)
@@ -24,6 +24,25 @@ RSpec.describe ReservationService, type: :model do
       expect(data.code).to eq('XXX12345678')
       expect(data.guest_email).to eq('wayne_woodbridge@bnb.com')
       expect(data.guest_phone_numbers).to eq('639123456789,639123456789')
+    end
+  end
+
+  context 'Update Reservation:' do
+    it 'should update fields if client send request with same reservation code' do
+      params = JSON.parse(payload1, symbolize_names: true)
+      req_client = "Airbnb"
+      req_code = params[:reservation_code]
+      req_guests = params[:guests]
+      origin_guests = 1
+
+      data = create(:reservation, client: req_client, code: req_code, guests: origin_guests)
+      expect(data.guests).to eq(origin_guests)
+
+      rsv = ReservationService.new(params)
+      rsv.run()
+      data.reload
+
+      expect(data.guests).to eq(req_guests)
     end
   end
 
